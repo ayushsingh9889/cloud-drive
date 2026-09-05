48bf66f2cfddd9b5617f65aa9d505b4cc42e8843
Ayush singh <ayush.singh121510@gmail.com>
2026-09-05 14:28:58 +0530

    Change CORS origin to allow all origins

--- backend/src/app.js
+++ backend/src/app.js
@@ -20,7 +20,7 @@ const app = express();
 app.use(helmet());
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+    origin: "*",
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
